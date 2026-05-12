3197ba27a4d6e5c7d027cecc6d3fa67c4efee4b8
yapiassamoi90-boop <yapiassamoi90@gmail.com>
2026-05-12 09:31:57 +0000

    Reformat server.js for consistency

--- server.js
+++ server.js
@@ -20,8 +20,7 @@ const client = new Client({
             '--no-sandbox',
             '--disable-setuid-sandbox',
             '--disable-dev-shm-usage'
-        ],
-        executablePath: process.env.CHROME_BIN || '/usr/bin/google-chrome'
+        ]
     }
 });
 
